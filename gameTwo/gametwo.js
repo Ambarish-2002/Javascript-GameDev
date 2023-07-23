12ca7e3d181a90a2c2ca792b84ceb388746a8832
Ambarish-2002 <amb.kshirsagar@gmail.com>
2023-07-23 17:40:05 +0530

    gameTwo finished

--- gameTwo/gametwo.js
+++ gameTwo/gametwo.js
@@ -2,7 +2,17 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 10;
+const slider = document.getElementById('gameSpeed');
+const span = document.getElementById('speedValue');
+
+let gameSpeed = 5;
+let gameFrame = 0;
+
+slider.addEventListener('change',
+function (e){
+    gameSpeed = e.target.value;
+    span.innerHTML = e.target.value;
+})
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = 'layer-1.png';
@@ -22,18 +32,44 @@ backgroundLayer5.src = 'layer-5.png';
 let layer1Roll_1 = 0;
 let layer1Roll_2 = 2400;
 
+class Layer {
+    
+    constructor(image,speedModifier){
+        this.x = 0;
+        this.y = 0;
+        this.width = 2400;
+        this.height = 700;
+        this.image = image;
+        this.speedModifier = speedModifier;
+        this.speed = gameSpeed*this.speedModifier;
+    }
+
+    update(){
+        this.speed = gameSpeed*this.speedModifier;
+        this.x = gameFrame*this.speed % this.width;
+    }
+
+    draw(){
+        ctx.drawImage(this.image,this.x,this.y,this.width,this.height);
+        ctx.drawImage(this.image,this.x+this.width,this.y,this.width,this.height);
+    }
+}
+
+const Layer_1 = new Layer(backgroundLayer1,0.2);
+const Layer_2 = new Layer(backgroundLayer2,0.4);
+const Layer_3 = new Layer(backgroundLayer3,0.6);
+const Layer_4 = new Layer(backgroundLayer4,0.8);
+const Layer_5 = new Layer(backgroundLayer5,1);
+
+let gameLayer = [Layer_1 , Layer_2, Layer_3,Layer_4 , Layer_5];
+
 function animate(){
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    ctx.drawImage(backgroundLayer4, layer1Roll_1 , 0);
-    ctx.drawImage(backgroundLayer4, layer1Roll_2 , 0);
-    
-    if(layer1Roll_1<-2400) layer1Roll_1 = 2400;
-    else layer1Roll_1-=gameSpeed;
-
-    if(layer1Roll_2<-2400) layer1Roll_2 = 2400;
-    else layer1Roll_2-=gameSpeed;
-
     requestAnimationFrame(animate);
-    
+    gameFrame--;
+    gameLayer.forEach(layer =>{
+        layer.update();
+        layer.draw();
+    });
 }
 animate();
