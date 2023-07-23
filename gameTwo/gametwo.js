63660a2b67eda02c4f948c570f0acb0e87174cbd
Ambarish-2002 <amb.kshirsagar@gmail.com>
2023-07-23 16:51:45 +0530

    Update gametwo.js

--- gameTwo/gametwo.js
+++ gameTwo/gametwo.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
+let gameSpeed = 10;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = 'layer-1.png';
@@ -18,11 +19,21 @@ backgroundLayer4.src = 'layer-4.png';
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'layer-5.png';
 
-let x = 0;
+let layer1Roll_1 = 0;
+let layer1Roll_2 = 2400;
 
 function animate(){
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    ctx.drawImage(backgroundLayer4, x , 0);
+    ctx.drawImage(backgroundLayer4, layer1Roll_1 , 0);
+    ctx.drawImage(backgroundLayer4, layer1Roll_2 , 0);
+    
+    if(layer1Roll_1<-2400) layer1Roll_1 = 2400;
+    else layer1Roll_1-=gameSpeed;
+
+    if(layer1Roll_2<-2400) layer1Roll_2 = 2400;
+    else layer1Roll_2-=gameSpeed;
+
     requestAnimationFrame(animate);
+    
 }
 animate();
