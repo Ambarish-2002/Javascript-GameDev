562af42fdad3321c53e323be5e6e9179edbc207a
Ambarish-2002 <amb.kshirsagar@gmail.com>
2023-07-26 01:30:39 +0530

    Update gameThree.js

--- gameThree/gameThree.js
+++ gameThree/gameThree.js
@@ -26,12 +26,28 @@ class Enemy{
         // this.curve = Math.random() * 6 + 0.1;
 
         // ENEMY TYPE 3
+        // this.enemyImage = new Image();
+        // this.enemyImage.src = 'enemy3.png';
+        // this.flapSpeed = Math.random() * 10;
+        // this.speed = Math.random() * 4 + 1;
+        // this.spriteWidth = 218;
+        // this.spriteHeight = 177;
+        // this.width = this.spriteWidth/2.5;
+        // this.height = this.spriteHeight/2.5;
+        // this.x = Math.random() * (canvas.width - this.width);
+        // this.y = Math.random() * (canvas.height - this.height);
+        // this.frame = 0;
+        // this.angle = Math.random()*3.14;
+        // this.angleSpeed = Math.random() * 2 + 0.5;
+        // this.curve = Math.random() * 200 + 10;
+
+        //Enemy 4 
         this.enemyImage = new Image();
-        this.enemyImage.src = 'enemy3.png';
+        this.enemyImage.src = 'enemy4.png';
         this.flapSpeed = Math.random() * 10;
         this.speed = Math.random() * 4 + 1;
-        this.spriteWidth = 218;
-        this.spriteHeight = 177;
+        this.spriteWidth = 213;
+        this.spriteHeight = 213;
         this.width = this.spriteWidth/2.5;
         this.height = this.spriteHeight/2.5;
         this.x = Math.random() * (canvas.width - this.width);
@@ -40,12 +56,22 @@ class Enemy{
         this.angle = Math.random()*3.14;
         this.angleSpeed = Math.random() * 2 + 0.5;
         this.curve = Math.random() * 200 + 10;
+        this.newX = Math.random() * (canvas.width - this.width);
+        this.newY = Math.random() * (canvas.height - this.height);
 
     }
 
     update(){
-        this.x = canvas.width/2 * Math.sin(this.angle * Math.PI/180) + (canvas.width/2 - this.width/2);
-        this.y =  canvas.height/2 * Math.cos(this.angle * Math.PI/180) + (canvas.height/2 - this.height/2);
+        if(gameFrame%60 == 0){
+            this.newX = Math.random() * (canvas.width - this.width);
+            this.newY = Math.random() * (canvas.height - this.height);
+        }
+        let dx = this.x - this.newX;
+        let dy = this.y - this.newY;
+        this.x-= dx/70;
+        this.y-= dy/70;
+        // this.x = canvas.width/2 * Math.sin(this.angle * Math.PI/180) + (canvas.width/2 - this.width/2);
+        // this.y =  canvas.height/2 * Math.cos(this.angle * Math.PI/180) + (canvas.height/2 - this.height/2);
         this.frame = Math.floor(gameFrame / 2 )%6;
         if(this.x < -this.width) this.x = canvas.width;
         this.angle+=this.angleSpeed;
